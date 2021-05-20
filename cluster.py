6daa9f108744d5481c0a1e6efa75707b7c41683b
Zongo Maqutu <MQTZON001@myuct.ac.za>
2021-05-20 02:03:23 +0200

    created main method and created point objects

---
+++ cluster.py
@@ -0,0 +1,20 @@
+import numpy as np
+class Cluster:
+    def __init__(self, centroid_id, centroid_x, centroid_y):
+        self.centroid_id = centroid_id
+        self.centroid_x = centroid_x
+        self.centroid_y = centroid_y
+
+    def get_centroid_id(self):
+        return self.centroid_id
+
+    def get_centroid_pos(self):
+        return '{}{}{}'.format(self.centroid_x, ',', self.centroid_y)
+
+    def calculate_distance(self):
+        np.sqrt(np.sum(x1-x2)**2)
+
+    def update_centroid(self):
+        return 1
+
+
---
+++ kmeans.py
@@ -0,0 +1,27 @@
+from point import Point
+from cluster import Cluster
+
+class kmeans:
+    ### list of all our data points
+    points_list = []
+    ### list of clusters
+    cluster_list = []
+    ### this is a mapping for cluster_id to a list of points belonging to that cluster
+    cluster_points = {}
+    def main():
+
+
+
+    if __name__ == "__main__":
+        main()
+    def generate_points_list(self):
+        point_map = {
+                1:(2,10),
+                2:(2,5),
+                3:(8,4),
+                4:(5,8),
+                5:(7,5),
+                6:(6,4),
+                7:(1,2),
+                8:(4,9)
+                }
